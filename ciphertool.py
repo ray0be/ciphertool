34a9408f3516f9f440b86816617f690faf78c07a
ray0be <victor.paysaut@gmail.com>
2018-01-02 21:12:35 +0100

    Menu reset

--- ciphertool.py
+++ ciphertool.py
@@ -1,9 +1,3 @@
 import functions as g
 
-#g.menu()
-
-from threefish import threefish_decrypt
-from threefish import threefish_encrypt
-
-print("Vous avez choisi le déchiffrement symétrique ThreeFish.")
-threefish_decrypt.run()
+g.menu()
